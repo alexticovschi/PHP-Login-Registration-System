6930f1c3a0f6beffe4a4c599a3fcc6a760085d4b
AlexTicovschi <alexmticovschi@gmail.com>
2018-01-31 20:53:09 +0000

    validating input fields

--- functions/functions.php
+++ functions/functions.php
@@ -39,12 +39,27 @@ function token_generator() {
 }
 
 
+function validation_errors($error_message) {
+	echo '
+		<div class="alert alert-danger alert-dismissible" role="alert">
+			<strong>Warning!</strong> ' . $error_message . '
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+	';
+}
 
 /* ======================== */
 /*   VALIDATION FUNCTIONS   */ 
 /* ======================== */
 
 function validate_user_registration() {
+	$errors = [];
+
+	$min = 3;
+	$max = 20;
+
 	if($_SERVER['REQUEST_METHOD'] == "POST") {
 		$first_name 		= clean($_POST['first_name']);
 		$last_name  		= clean($_POST['last_name']);
@@ -53,6 +68,44 @@ function validate_user_registration() {
 		$password 			= clean($_POST['password']);
 		$confirm_password 	= clean($_POST['confirm_password']);
 
+		if(strlen($first_name) < $min) {
+			$errors[] = "Your first name cannot be less than {$min} characters";
+		}
+
+		if(strlen($first_name) > $max) {
+			$errors[] = "Your first name cannot be greater than {$max} characters";
+		}
+
+		if(strlen($last_name) < $min) {
+			$errors[] = "Your last name cannot be less than {$min} characters";
+		}
+
+		if(strlen($last_name) > $max) {
+			$errors[] = "Your last name cannot be greater than {$max} characters";
+		}
+
+		if(strlen($username) < $min) {
+			$errors[] = "Your username cannot be less than {$min} characters";
+		}
+
+		if(strlen($username) > $max) {
+			$errors[] = "Your username cannot be greater than {$max} characters";
+		}
+
+		// if(strlen($email) > $max) {
+		// 	$errors[] = "Your email cannot be greater than {$max} characters";
+		// }
+
+		if($password !== $confirm_password) {
+			$errors[] = "Your password fields do not match";
+		}
+
+
+		if(!empty($errors)) {
+			foreach ($errors as $error) { 
+				validation_errors($error);
+			}
+		}
 	}
 }
 
