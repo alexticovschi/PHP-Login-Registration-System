69b7122c8f23403d7ae2f0a8d63bad3433615b36
AlexTicovschi <alexmticovschi@gmail.com>
2019-04-09 10:44:35 +0100

    logout feature

--- includes/nav.php
+++ includes/nav.php
@@ -14,7 +14,16 @@
             <li class="active"><a href="index.php">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#contact">Contact</a></li>
-            <li><a href="login.php">Login</a></li>
+            <li>
+              <?php 
+                if(logged_in()) {
+                  echo '<a href="logout.php">Logout</a>';
+                } 
+                else {
+                  echo '<a href="login.php">Login</a>';
+                } 
+              ?>
+            </li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
